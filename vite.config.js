14782033b1c7173c2c98bd13b48dc862b96e61ed
MICHAELROMERORAMOS <145713478+MICHAELROMERORAMOS@users.noreply.github.com>
2026-09-19 23:23:51 +0200

    Improve GitHub Pages browser compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/RestOS-Plus/',
   plugins: [react()],
+  build: {
+    target: 'es2018',
+  },
   server: {
     port: 5173,
     strictPort: true,
